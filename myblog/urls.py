46d1315b2c020f8bbf80d01481fadf0cfcda212c
CarlosTenorio <carlostenorioperez@hotmail.com>
2018-03-03 22:03:41 +0100

    Add select lenguage on navbar

--- myblog/urls.py
+++ myblog/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += i18n_patterns(
