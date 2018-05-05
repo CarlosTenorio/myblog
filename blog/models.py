7071dc0604369342a8c80dabf09cab5976ebc45c
CarlosTenorio <carlostenorioperez@hotmail.com>
2018-05-05 16:48:55 +0200

    internationalization removed.

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     author = models.ForeignKey(User, related_name="user_post",
         on_delete=models.PROTECT, verbose_name='Autor')
-    title_en = models.CharField(max_length=200, null=True)
+    title_en = models.CharField(max_length=200, null=True, blank=True)
     title_es = models.CharField(max_length=200, null=True)
-    preview_en = models.TextField(default="")
+    preview_en = models.TextField(blank=True)
     preview_es = models.TextField(default="")
-    text_en = models.TextField(default="")
+    text_en = models.TextField(blank=True)
     text_es = models.TextField(default="")
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
@@ -20,4 +20,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title_en
+        return self.title_es
